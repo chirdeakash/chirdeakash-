0daa3ae3f5e72f98fa316da62438348657f04f9c
chirdeakash <77576312+chirdeakash@users.noreply.github.com>
2021-01-17 20:01:18 +0530

    R programming intro

---
+++ introduction to R programming.R
@@ -0,0 +1,11 @@
+#Introduction
+print("Welcome to Analytics Edge class")
+a<-("Welcome to Analytics Edge class")
+#a<-(print("Welcome to Analytics Edge class"))
+a
+
+#plotting
+X=1:5
+Y=:10
+plot(X,Y)
+
